373ff2dca48d212e06044a935e03214211fe614d
Chris AtLee <chris.atlee@shopify.com>
2021-12-21 17:42:07 -0500

    FINALLY

--- 2021/ruby/day19.rb
+++ 2021/ruby/day19.rb
@@ -4,6 +4,48 @@ require "set"
 require "aoc"
 require "matrix"
 
+SIMPLE_REPORT = <<~SIMPLE_REPORT
+--- scanner 0 ---
+-1,-1,1
+-2,-2,2
+-3,-3,3
+-2,-3,1
+5,6,-4
+8,0,7
+
+--- scanner 0 ---
+1,-1,1
+2,-2,2
+3,-3,3
+2,-1,3
+-5,4,-6
+-8,-7,0
+
+--- scanner 0 ---
+-1,-1,-1
+-2,-2,-2
+-3,-3,-3
+-1,-3,-2
+4,6,5
+-7,0,8
+
+--- scanner 0 ---
+1,1,-1
+2,2,-2
+3,3,-3
+1,3,-2
+-4,-6,5
+7,0,8
+
+--- scanner 0 ---
+1,1,1
+2,2,2
+3,3,3
+3,1,2
+-6,-4,-5
+0,7,-8
+SIMPLE_REPORT
+
 SAMPLE_REPORT = <<~SAMPLE
 --- scanner 0 ---
 404,-588,-901
@@ -144,6 +186,14 @@ SAMPLE_REPORT = <<~SAMPLE
 SAMPLE
 
 Pos = Struct.new(:x, :y, :z) do
+  def self.from_m(m)
+    Pos.new(*m.column(0))
+  end
+
+  def mandist
+    self.x.abs + self.y.abs + self.z.abs
+  end
+
   def +(o)
     Pos.new(self.x + o.x, self.y + o.y, self.z + o.z)
   end
@@ -157,11 +207,19 @@ Pos = Struct.new(:x, :y, :z) do
   end
 
   def transform(m)
-    Pos.new(*(m * Matrix.column_vector([x, y, z])))
+    Pos.from_m(m * Matrix.column_vector([x, y, z]))
   end
 
   def <=>(o)
     [self.x, self.y, self.z] <=> [o.x, o.y, o.z]
+  end
+
+  def to_v
+    Matrix.column_vector(to_a)
+  end
+
+  def to_a
+    [self.x, self.y, self.z]
   end
 end
 
@@ -170,6 +228,7 @@ def parse_reports(input)
   reports = []
   scanner_id = nil
   input.lines.each do |line|
+    next if line.strip == ""
     if m = /--- scanner (\d+) ---/.match(line)
       unless report_lines.empty?
         r = ScannerReport.new(report_lines)
@@ -188,6 +247,39 @@ def parse_reports(input)
   reports
 end
 
+TRANSFORMS = [
+  Matrix::I(3)
+]
+
+# Create a matrix to rotate around x/y/z
+def rotate(x, y, z)
+  Matrix[
+    [x, -z, -y],
+    [z,  y,  x],
+    [y, -x,  z],
+  ]
+end
+
+
+# Rotate around the z axis 3 times to get our four different "ups"
+3.times do
+  TRANSFORMS << TRANSFORMS[-1] * rotate(0,0,1)
+end
+
+# Rotate each of the 4 "ups" around the y axis three times to get right, back, left
+TRANSFORMS[0..3].each do |m|
+  3.times do
+    m = m * rotate(0,1,0)
+    TRANSFORMS << m
+  end
+end
+
+# Rotate forward and backward around the x-axis for facing up and down
+TRANSFORMS[0..3].each do |m|
+  TRANSFORMS << m * rotate(1,0,0)
+  TRANSFORMS << m * rotate(1,0,0) * rotate(1,0,0) * rotate(1,0,0)
+end
+
 class Scanner
   attr_accessor :pos, :beacons
 
@@ -204,37 +296,72 @@ class Scanner
     end
   end
 
-  def merge_report(report)
+  def find_matches(test_pair, real_pair)
+    real_pair = real_pair.sort
+
+    TRANSFORMS.map do |m|
+      mapped_pairs = test_pair.map { |t| t.transform(m) }.sort
+      offset = real_pair.first - mapped_pairs.first
+
+      # Now check that the other matches the other real pair
+      if mapped_pairs.last + offset == real_pair.last
+        [m, offset]
+      end
+    end.compact
+  end
+
+  def merge_report(report, min_matches)
     # Assume we're facing the same direction for now
     # Find an offset that we can use to shift all the becaons by
     my_becaon_distances = beacon_distances
 
     offset = nil
+    transform = nil
     report.beacon_distances.each do |d, pairs|
       unless pairs.length == 1
-        puts "distance #{d} is ambiguous; skipping"
+        # puts "distance #{d} is ambiguous; skipping"
         next
       end
       if (my_pairs = my_becaon_distances[d]) && my_pairs.length == 1
-        b1 = pairs.first.sort.first
-        b2 = my_pairs.first.sort.first
-        offset = b2-b1
-        break
+        test_pair = pairs.first
+        real_pair = my_pairs.first
+
+        transforms = find_matches(test_pair, real_pair)
+        next unless transforms.length > 0
+        transforms.each do |t, o|
+          # puts "Found transform:#{t}; offset:#{o}"
+
+          mapped_beacons = Set.new(report.beacons.map { |b| b.transform(t) + o })
+          # puts "beacons: #{beacons}"
+          # puts "mapped : #{mapped_beacons}"
+          overlap = @beacons & mapped_beacons
+          if overlap.length >= min_matches
+            offset = o
+            transform = t
+            break
+          else
+            # puts "#{overlap.length} not enough matches; trying another option"
+          end
+        end
+
+        break if offset && transform
       end
     end
 
-    unless offset
-      puts "couldn't find offset"
+    unless offset && transform
+      # puts "couldn't find offset for"
+      # puts report.beacons
+      # puts @beacons
       return false
     end
 
-    puts "offset: #{offset}"
+    # puts "offset: #{offset}; transform: #{transform}"
 
     report.beacons.each do |b|
-      m = b + offset
+      m = b.transform(transform) + offset
       @beacons << m
     end
-    return true
+    return offset
   end
 end
 
@@ -288,33 +415,79 @@ class Day19 < Minitest::Test
     assert_equal r1.beacon_distances.keys.sort, r2.beacon_distances.keys.sort
 
     s = Scanner.new(r1)
-    s.merge_report(r2)
+    s.merge_report(r2, 3)
     assert_equal 3, s.beacons.length
   end
 
-  def part1(input)
+  def test_merging
+    reports = parse_reports(SIMPLE_REPORT)
+
+    s = Scanner.new(reports[0])
+
+    reports[1..].each do |r|
+      # puts
+      # puts "merging: #{r.beacons}"
+      assert s.merge_report(r, 6)
+    end
+
+    assert_equal 6, s.beacons.length
+  end
+
+  def part1(input, min_matches)
     reports = parse_reports(input)
 
-    0
+    s = Scanner.new(reports[0])
+    to_merge = reports[1..]
+    while !to_merge.empty?
+      r = to_merge.shift
+      unless s.merge_report(r, min_matches)
+        # puts "Couldn't merge #{r}"
+        to_merge << r
+      end
+    end
+    s.beacons.length
+  end
+
+  def test_part1_simple
+    assert_equal 6, part1(SIMPLE_REPORT, 6)
   end
 
   def test_part1_sample
-    assert_equal 0, part1(SAMPLE_REPORT)
+    assert_equal 79, part1(SAMPLE_REPORT, 12)
   end
 
   def test_part1
-    assert_equal 0, part1(DAY19_text)
+    assert_equal 430, part1(DAY19_text, 12)
   end
 
-  def part2(input)
-    0
+  def part2(input, min_matches=12)
+    positions = []
+    reports = parse_reports(input)
+
+    s = Scanner.new(reports[0])
+    to_merge = reports[1..]
+    while !to_merge.empty?
+      r = to_merge.shift
+      if pos = s.merge_report(r, min_matches)
+        positions << pos
+      else
+        to_merge << r
+      end
+    end
+
+    positions.combination(2).map { |p1, p2| (p1-p2).mandist }.max
   end
 
   def test_part2_sample
-    assert_equal 0, part2(SAMPLE_REPORT)
+    assert_equal 3621, part2(SAMPLE_REPORT,)
   end
 
   def test_part2
-    assert_equal 0, part2(DAY19_text)
+    assert_equal 11860, part2(DAY19_text)
+  end
+
+  def test_transforms
+    assert_equal 24, TRANSFORMS.length
+    assert_equal 24, Set.new(TRANSFORMS).length
   end
 end
