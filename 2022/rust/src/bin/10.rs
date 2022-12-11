3dfc957acfb6e1f2b6ab37bcd9c7ded2b55c9103
Chris AtLee <chris@atlee.ca>
2022-12-11 14:46:47 -0500

    add alternate implemenation for day 10 to find the bug!

--- 2022/rust/src/bin/10.rs
+++ 2022/rust/src/bin/10.rs
@@ -13,6 +13,7 @@ struct Cpu {
     cycle: i32,
     ip: usize,
     instructions: Vec<Instruction>,
+    instr_cycles: i32,
 }
 
 impl Cpu {
@@ -22,7 +23,14 @@ impl Cpu {
             .filter_map(|line| line.parse().ok())
             .collect::<Vec<Instruction>>();
 
-        Self::new(instructions)
+        // This was a major bug. If the first instruction is an addx, we need to initialize
+        // instr_cycles properly
+        let mut cpu = Self::new(instructions);
+        cpu.instr_cycles = match cpu.instructions[0] {
+            Instruction::Addx(_) => 1,
+            _ => 0,
+        };
+        cpu
     }
     fn new(instructions: Vec<Instruction>) -> Self {
         Cpu {
@@ -30,12 +38,50 @@ impl Cpu {
             cycle: 1,
             ip: 0,
             instructions,
+            instr_cycles: 0,
         }
     }
 
+    // This implementation is cycle-focused; we update the ip when we're finished the cycles for an
+    // Instruction
+    fn run_to_cycle2(&mut self, target_cycle: i32) -> i32 {
+        while self.cycle < target_cycle {
+            eprintln!(
+                "{}: {} (x={})",
+                self.cycle,
+                self.instructions.get(self.ip).unwrap(),
+                self.x
+            );
+            self.cycle += 1;
+            if self.instr_cycles > 0 {
+                self.instr_cycles -= 1;
+                continue;
+            }
+            match self.instructions.get(self.ip) {
+                Some(Instruction::Noop) => {
+                    self.ip += 1;
+                }
+                Some(Instruction::Addx(n)) => {
+                    self.x += n;
+                    self.ip += 1;
+                    eprintln!("  x += {} -> {}", n, self.x);
+                }
+                None => panic!("program ran past the end"),
+            }
+            self.instr_cycles = match self.instructions.get(self.ip) {
+                Some(Instruction::Noop) => 0,
+                Some(Instruction::Addx(_)) => 1,
+                _ => panic!("program ran past the end"),
+            }
+        }
+        self.x
+    }
+
+    // This implementation is instruction-focused; we add cycles after we run instuctions
     fn run_to_cycle(&mut self, target_cycle: i32) -> i32 {
         while self.ip < self.instructions.len() {
             // TODO: I struggled figuring out this condition
+            // If target_cycle is 20, we'll stop when cycle is 19
             if self.cycle >= target_cycle - 1 {
                 break;
             }
@@ -115,6 +161,17 @@ mod tests {
     }
 
     #[test]
+    fn test_cpu_simple2() {
+        let mut cpu = Cpu::from_str("noop\naddx 3\naddx -5\n");
+        cpu.run_to_cycle(1);
+        assert_eq!(cpu.run_to_cycle2(1), 1);
+        assert_eq!(cpu.run_to_cycle2(2), 1);
+        assert_eq!(cpu.run_to_cycle2(3), 1);
+        assert_eq!(cpu.run_to_cycle2(4), 4);
+        assert_eq!(cpu.run_to_cycle2(5), 4);
+    }
+
+    #[test]
     fn test_cpu_example() {
         let input = advent_of_code::read_file("examples", 10);
         let mut cpu = Cpu::from_str(&input);
@@ -124,6 +181,18 @@ mod tests {
         assert_eq!(cpu.run_to_cycle(140), 21);
         assert_eq!(cpu.run_to_cycle(180), 16);
         assert_eq!(cpu.run_to_cycle(220), 18);
+    }
+
+    #[test]
+    fn test_cpu_example2() {
+        let input = advent_of_code::read_file("examples", 10);
+        let mut cpu = Cpu::from_str(&input);
+        assert_eq!(cpu.run_to_cycle2(20), 21);
+        assert_eq!(cpu.run_to_cycle2(60), 19);
+        assert_eq!(cpu.run_to_cycle2(100), 18);
+        assert_eq!(cpu.run_to_cycle2(140), 21);
+        assert_eq!(cpu.run_to_cycle2(180), 16);
+        assert_eq!(cpu.run_to_cycle2(220), 18);
     }
 
     #[test]
